bc0cfa8a007daca25b941a250841aad4297a9b73
David Boddie <david@boddie.org.uk>
2015-11-01 17:49:32 +0100

    Added support for encoding and serialisation of compressed data. Added a check that compares decoded data against the original.

--- prefix.py
+++ prefix.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import pprint, sys
 
 class Leaf:
 
@@ -12,6 +12,11 @@ class Leaf:
     def dump(self, n):
     
         print n, self.value
+    
+    def serialise(self, n, encdict, node_array, bits):
+    
+        encdict[self.value] = (n, bits)
+        node_array += [-self.value]
 
 class Node:
 
@@ -34,6 +39,27 @@ class Node:
         else:
             self.leaf0.dump(n + "1")
             self.leaf1.dump(n + "0")
+    
+    def serialise(self, n, encdict, node_array, bits = 0):
+    
+        i = len(node_array)
+        
+        # Add an entry to be filled in later.
+        node_array += [0]
+        
+        v0 = n
+        v1 = n | (1 << bits)
+        
+        if isinstance(self.leaf0, Leaf):
+            self.leaf0.serialise(v0, encdict, node_array, bits + 1)
+            # Fill in the offset that points to the second leaf or node.
+            node_array[i] = len(node_array) - i
+            self.leaf1.serialise(v1, encdict, node_array, bits + 1)
+        else:
+            self.leaf1.serialise(v0, encdict, node_array, bits + 1)
+            # Fill in the offset that points to the second leaf or node.
+            node_array[i] = len(node_array) - i
+            self.leaf0.serialise(v1, encdict, node_array, bits + 1)
 
 def sorted(count):
 
@@ -43,16 +69,20 @@ def sorted(count):
         values.append((count[i], i))
         i += 1
     
+    # Discard values that didn't occur.
     values = filter(lambda (n, i): n != 0, values)
+    # Sort the values in increasing order of frequency.
     values.sort()
     
     nodes = map(lambda (n, i): Leaf(i, n), values)
+    total = len(nodes)
     
     while len(nodes) > 1:
     
         n0 = nodes.pop(0)
         n1 = nodes.pop(0)
         node = Node(n0, n1)
+        total += 1
         i = 0
         while i < len(nodes):
             if node.weight < nodes[i].weight:
@@ -62,11 +92,19 @@ def sorted(count):
         else:
             nodes.append(node)
     
-    nodes[0].dump()
+    encdict = {}
+    node_array = []
+    nodes[0].serialise(0, encdict, node_array)
+    
+    print len(node_array)
+    print min(node_array), max(node_array), total
+    
+    return encdict, node_array
 
 def encode(input_file, output_file):
 
     count = [0] * 256
+    data = []
     
     while True:
     
@@ -76,9 +114,65 @@ def encode(input_file, output_file):
         
         i = ord(ch)
         count[i] += 1
+        data.append(i)
     
-    sorted(count)
+    encdict, node_array = sorted(count)
+    
+    bit = 0
+    c = 0
+    output_data = []
+    
+    for i in data:
+    
+        v, l = encdict[i]
+        while l > 0:
+            c = c | ((v & 1) << bit)
+            v = v >> 1
+            l -= 1
+            bit += 1
+            if bit == 8:
+                output_file.write(chr(c))
+                output_data.append(c)
+                bit = 0
+                c = 0
+    
+    if bit != 0:
+        output_file.write(chr(c))
+        output_data.append(c)
+    
+    # Decode to test.
+    decode_(output_data, node_array, data, len(data))
 
+def decode_(input_data, node_array, expected_output, size = 32768):
+
+    bit = 0
+    c = 0
+    data = []
+    offset = 0
+    i = 0
+    
+    while len(data) < size:
+    
+        if bit == 0:
+            c = input_data[i]
+            i += 1
+        
+        if c & 1 == 0:
+            offset += 1
+        else:
+            offset += node_array[offset]
+        
+        if node_array[offset] <= 0:
+            data.append(-node_array[offset])
+            if data[-1] != expected_output[len(data)-1]:
+                sys.stderr.write("Decoding check failed at offset %x.\n" % (len(data)-1))
+                sys.exit(1)
+            offset = 0
+        
+        c = c >> 1
+        bit = (bit + 1) % 8
+    
+    return data
 
 if __name__ == "__main__":
 
