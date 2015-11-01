a6834fbbef55004e7f285977d1f58fc88c27c2cd
David Boddie <david@boddie.org.uk>
2015-11-01 19:01:34 +0100

    Committing incomplete pending code.

--- huffman.py
+++ huffman.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import marshal, sys
 
 class Node:
 
@@ -19,51 +19,127 @@ class Node:
     
         return self.less[0] + self.greater[0]
 
-def find_codes(node, current, bit, dictionary):
+class BitWriter:
 
+    def __init__(self):
+    
+        self.header = None
+        self.data = []
+        self.current = 0
+        self.bit = 0
+    
+    def writeHeader(self, codes):
+    
+        self.header = codes.items()
+        self.header.sort()
+    
+    def write(self, value, bits = None):
+    
+        if bits == None:
+        
+            bits = 0
+            v = value
+            while v != 0:
+                v = v >> 1
+                bits += 1
+        
+        while bits > 0:
+        
+            if value & 1 == 1:
+                self.current = self.current | (1 << self.bit)
+            
+            self.bit += 1
+            
+            if self.bit == 8:
+                self.bit = 0
+                self.data.append(chr(self.current))
+                self.current = 0
+            
+            bits -= 1
+            value = value >> 1
+    
+    def __str__(self):
+    
+        if self.bit != 0:
+            self.data.append(chr(self.current))
+            self.bit = 0
+            self.current = 0
+        
+        return marshal.dumps((self.header, "".join(self.data)))
+
+class BitReaderError(Exception):
+    pass
+
+class BitReader:
+
+    def __init__(self, data):
+    
+        self.current = 0
+        self.bit = 0
+        self.header, self.data = marshal.loads(data)
+        
+        self.root = []
+        
+        for (bits, value), character in self.header:
+            self.storeCharacter(bits, value, character, self.root)
+    
+    def storeCharacter(self, bits, value, character, parent):
+    
+        ###
+    
+    def read(self):
+    
+        
+            value = 0
+            current = self.data[self.current]
+            
+            while bits > 0:
+            
+                if current & (1 << self.bit) != 0:
+                    value = value | (1 << self.bit)
+                
+                self.bit += 1
+                
+                if self.bit == 8:
+                    self.bit = 0
+                    self.current += 1
+                    current = self.data[self.current]
+                
+                bits -= 1
+    
+    def __str__(self):
+    
+        return "".join(map(chr, self.data))
+
+def find_codes(node, current, bit, dictionary, reverse):
+
+    # Descend the tree of nodes, assigning codes and the number of bits
+    # required them to each node.
+    
     if isinstance(node.less[1], Node):
-        find_codes(node.less[1], current, bit + 1, dictionary)
+        less_max_bits = find_codes(node.less[1], current, bit + 1, dictionary,
+                                   reverse)
     else:
-        dictionary[(current, bit + 1)] = node.less[1]
+        code = current
+        less_max_bits = bit + 1
+        dictionary[(less_max_bits, code)] = node.less[1]
+        reverse[node.less[1]] = code
     
     if isinstance(node.greater[1], Node):
-        find_codes(node.greater[1], current | (1 << bit), bit + 1, dictionary)
+        greater_max_bits = find_codes(node.greater[1], current | (1 << bit),
+                                    bit + 1, dictionary, reverse)
     else:
-        dictionary[(current | (1 << bit), bit + 1)] = node.greater[1]
+        code = current | (1 << bit)
+        greater_max_bits = bit + 1
+        dictionary[(greater_max_bits, code)] = node.greater[1]
+        reverse[node.greater[1]] = code
+    
+    return max(less_max_bits, greater_max_bits)
 
-def bin(value, bits):
+def encode(input_file, output_file):
 
-    output = ""
-    
-    while bits > 0:
-        if value & 1 == 0:
-            output += "0"
-        else:
-            output += "1"
-        value = value >> 1
-        bits -= 1
-    
-    return output
-
-
-if __name__ == "__main__":
-
-    if len(sys.argv) == 1:
-    
-        in_f = sys.stdin
-        out_f = sys.stdout
-    
-    elif len(sys.argv) == 3:
-    
-        in_f = open(sys.argv[1], "rb")
-        out_f = open(sys.argv[2], "wb")
-    
-    else:
-        sys.stderr.write("Usage: %s [<input file> <output file>]\n" % sys.argv[0])
-        sys.exit(1)
-    
-    text = in_f.read()
-    in_f.close()
+    text = input_file.read()
+    input_file.close()
     
     frequencies = {}
     for character in text:
@@ -103,9 +179,68 @@ if __name__ == "__main__":
             table.append((weight, node))
     
     codes = {}
-    find_codes(node, 0, 0, codes)
+    reverse = {}
+    max_bits = find_codes(node, 0, 0, codes, reverse)
     
-    for (code, bits), character in codes.items():
-        print bin(code, bits), repr(character)
+    # Write table.
+    #
+    # Number of entries (1 byte).
+    b = BitWriter()
+    b.write(len(codes), 8)
+    b.writeHeader(codes)
+    
+    #for item in range(len(codes)):
+    #    try:
+    #        character = codes[item]
+    #    except KeyError:
+    #        # Encode a branch node as a null byte.
+    #        character = "\x00"
+    #    b.write(ord(character), 8)
+    #    print hex(item), repr(character)
+    
+    for c in text:
+        b.write(reverse[c])
+    
+    output_file.write(str(b))
+
+def decode(input_file, output_file):
+
+    data = input_file.read()
+    
+    b = BitReader(data)
+    
+
+if __name__ == "__main__":
+
+    args = sys.argv[1:]
+    
+    command = None
+    input_file = sys.stdin
+    output_file = sys.stdout
+    
+    try:
+        command = args.pop(0)
+        input_file = args.pop(0)
+        output_file = args.pop(0)
+    
+    except IndexError:
+    
+        if not command:
+            sys.stderr.write("Usage: %s --encode|--decode [<input file> <output file>]\n" % sys.argv[0])
+            sys.exit(1)
+    
+    if input_file != sys.stdin:
+        if input_file == "-":
+            input_file = sys.stdin
+        else:
+            input_file = open(input_file, "rb")
+    
+    if output_file != sys.stdout:
+        output_file = open(output_file, "wb")
+    
+    if command == "--encode":
+        encode(input_file, output_file)
+    else:
+        decode(input_file, output_file)
     
     sys.exit()
